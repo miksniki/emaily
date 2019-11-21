790e96f60ba2722f7eac5fe96abd44c411d4ffee
Nikita Mikson <nikita.mikson@gmail.com>
2019-11-21 17:22:55 +0200

    fix

--- services/passport.js
+++ services/passport.js
@@ -19,8 +19,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-  proxy: true
+  callbackURL: 'https://frozen-springs-24316.herokuapp.com/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done) => {
   User.findOne({ googleId: profile.id }).
